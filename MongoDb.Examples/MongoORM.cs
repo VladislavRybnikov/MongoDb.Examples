e2c3eff2496adfa04afdddbe0f29840f9de8a93c
Vladyslav Rybnikov <vladyslav_rybnikov@epam.com>
2020-01-20 18:13:30 +0200

    Add project files.

---
+++ MongoDb.Examples/MongoORM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDb.Examples
+{
+
+    public interface IRepository<T> where T : class
+    {
+        void Insert(T value);
+    }
+
+    public abstract class MongoDbContext
+    {
+        private readonly Lazy<IMongoDatabase> _db;
+
+        protected MongoDbContext(string dbName)
+        {
+            var client = new MongoClient();
+            _db = new Lazy<IMongoDatabase>(() => client.GetDatabase(dbName));
+
+            var props = GetType().GetProperties();
+            props.Where(p => p.PropertyType.Name == "MongoDbSet`1")
+                .ToList().ForEach(SetProp);
+        }
+
+        private void SetProp(PropertyInfo p)
+        {
+            var inst = Activator.CreateInstance(p.PropertyType, _db);
+
+            p.SetValue(this, inst);
+        }
+    }
+
+    public class MongoDbSet<T> : IRepository<T>, IEnumerable<T> where T : class
+    {
+        private readonly Lazy<IMongoDatabase> _db;
+        private readonly Lazy<IMongoCollection<T>> _collection;
+
+        public MongoDbSet(Lazy<IMongoDatabase> db)
+        {
+            var t = typeof(T);
+            _db = db;
+            _collection = new Lazy<IMongoCollection<T>>(() => _db.Value.GetCollection<T>(t.Name + "s"));
+        }
+
+        public void Insert(T value)
+        {
+            _collection.Value.InsertOne(value);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _collection.Value.Find(new BsonDocument(), new FindOptions
+            {
+                ShowRecordId = false,
+            }).ToList().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ MongoDb.Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MongoDB.Driver;
+
+namespace MongoDb.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var db = new MyMongoDbContext();
+
+            db.Users.ToList().ForEach(x => Console.WriteLine(x.Name));
+        }
+    }
+
+    public class MyMongoDbContext : MongoDbContext
+    {
+        public MyMongoDbContext() : base("Test")
+        {
+        }
+
+        public MongoDbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+
+}
